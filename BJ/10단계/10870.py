109b680100e986def5fb8e9c025c094659f358d8
LDJ <ehdwnsdl1210@gmail.com>
2021-09-15 20:05:34 +0900

    0915 commit

---
+++ BJ/10단계/10870.py
@@ -0,0 +1,18 @@
+N = int(input())
+
+def fibo(n):
+    if n == 1:
+        return 1
+    elif n == 0:
+        return 0
+    else:
+        return fibo(n-1) + fibo(n-2)
+
+print(fibo(N))
+
+
+# def fibo(n):
+#     if n <= 1: return n
+#     return fibo(n-1) + fibo(n-2)
+
+# print(fibo(int(input())))
