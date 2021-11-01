02c85b3d3ea4a8f8f3116f6d4f8b3fd18e3160ae
LDJ <ehdwnsdl1210@gmail.com>
2021-11-02 01:35:23 +0900

    1102 commit

--- BJ/15단계/1003.py
+++ BJ/15단계/1003.py
@@ -1,29 +1,30 @@
+import sys
+sys.stdin = open('input.txt')
+
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
-    dp = [0] * (N+1)
+
     if N == 0:
         print(1, 0)
     elif N == 1:
         print(0, 1)
     else:
-        dp[N] = 1
+        result = [0] * (N + 1)
+        result[N] = 1
         for i in range(N, 1, -1):
-            dp[N-1] += 1
-            dp[N-2] += 1
-        print(dp[0], dp[1])
-
+            if result[i] != 0:
+                result[i-1] += result[i]
+                result[i-2] += result[i]
+        print(result[0], result[1])
 
 # def fibo(n):
-#     global zero
-#     global one
-#
 #     if n == 0:
-#         zero += 1
+#         result[0] += 1
 #         return
 #
 #     elif n == 1:
-#         one += 1
+#         result[1] += 1
 #         return
 #
 #     else:
@@ -32,9 +33,7 @@ for tc in range(1, T+1):
 #         return
 #
 # T = int(input())
-#
 # for tc in range(1, T+1):
-#     zero = 0
-#     one = 0
+#     result = [0, 0]
 #     fibo(int(input()))
-#     print(zero, one)
+#     print(*result)
--- BJ/15단계/1149.py
+++ BJ/15단계/1149.py
@@ -1,18 +1,2 @@
 import sys
-
-N = int(sys.stdin.readline())
-dp = [0] * N
-idx = -1
-for tc in range(1, N+1):
-    color_cost = list(map(int, sys.stdin.readline().split()))
-    # print(color_cost)
-
-    if color_cost.index((min(color_cost))) != idx:
-        dp[tc] = min(color_cost)
-    else:
-        color_cost[color_cost.idx(min(color_cost))]
-        if color_cost.index((min(color_cost))) != idx:
-            dp[tc] = min(color_cost)
-        else:
-            dp[tc] = max(color_cost)
-
+sys.stdin = open('input.txt')
