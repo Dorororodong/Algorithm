aa9979451d1c447596de9dd3b40040e335f8a7ee
LDJ <ehdwnsdl1210@gmail.com>
2021-12-27 01:00:28 +0900

    1227 commit

--- BJ/24단계/1012.py
+++ BJ/24단계/1012.py
@@ -1,36 +1,42 @@
 import sys
+from collections import deque
 sys.stdin = open('input.txt')
 
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 
-def DFS(x, y):
-    cabbage[x][y] = 2
+def BFS(x, y):
+    Q = deque()
+    Q.append((x, y))
+    visited[x][y] = 1
 
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
+    while Q:
+        x, y = Q.popleft()
 
-        if 0 <= nx < N and 0 <= ny < M and cabbage[nx][ny] == 1:
-            DFS(nx, ny)
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < N and 0 <= ny < M and cabbage[nx][ny] == 1 and visited[nx][ny] == 0:
+                visited[nx][ny] = 1
+                Q.append((nx, ny))
 
 T = int(input())
 
 for tc in range(1, T+1):
-    M, N, K = map(int, input().split())
+    M, N, K = map(int, sys.stdin.readline().rstrip().split())     # 가로, 세로, 배추 심어져있는 개수
     cabbage = [[0] * M for _ in range(N)]
     visited = [[0] * M for _ in range(N)]
 
     for _ in range(K):
-        X, Y = map(int, input().split())
+        X, Y = map(int, sys.stdin.readline().rstrip().split())
         cabbage[Y][X] = 1
-        visited[Y][X] = 1
 
     cnt = 0
     for i in range(N):
         for j in range(M):
-            if cabbage[i][j] == 1:
+            if cabbage[i][j] == 1 and visited[i][j] == 0:
                 cnt += 1
-                DFS(i, j)
+                BFS(i, j)
 
     print(cnt)
