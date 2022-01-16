5edd528e6cbc03b1e711a3ec86837fb373114f77
LDJ <ehdwnsdl1210@gmail.com>
2022-01-16 21:49:01 +0900

    220116 commit

--- BJ/6단계/1065.py
+++ BJ/6단계/1065.py
@@ -1,1 +1,20 @@
+def hansoo(n):
+    global cnt
+    for i in range(1, n+1):
+        if i <= 99:
+            cnt += 1
 
+        elif i == 1000:
+            pass
+
+        else:
+            if (i // 100 - (i % 100) // 10) == ((i % 100) // 10 - i % 10):
+                cnt += 1
+
+import sys
+N = int(sys.stdin.readline().rstrip())
+cnt = 0
+
+hansoo(N)
+
+print(cnt)
