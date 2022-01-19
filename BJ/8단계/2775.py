f90eca167a76c695de7153dbbb3f270c15c868b2
LDJ <ehdwnsdl1210@gmail.com>
2022-01-19 19:32:56 +0900

    220119 commit

--- BJ/8단계/2775.py
+++ BJ/8단계/2775.py
@@ -1,15 +1,36 @@
-def below_sum(k, n):
-    for _ in range(k):
-
-    return below_sum()
-
 import sys
 
 T = int(sys.stdin.readline().rstrip())
 
-for _ in range(1, T+1):
-    k = int(sys.stdin.readline().rstrip())  # 층
-    n = int(sys.stdin.readline().rstrip())  # 호
+floor = [[i for i in range(1, 15)]]
 
-    zero = [i+1 for i in range(n)]
-    below_sum(k, n)
+for i in range(14):
+    up_floor = []
+    for j in range(14):
+        up_floor.append(sum(floor[i][:j+1]))
+    floor.append(up_floor)
+
+for tc in range(1, T+1):
+    k = int(sys.stdin.readline().rstrip())
+    n = int(sys.stdin.readline().rstrip())
+
+    print(floor[k][n-1])
+
+'''
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    k = int(input())
+    n = int(input())
+
+    people = [i for i in range(n + 1)]
+
+    for i in range(k):
+        for j in range(1, n + 1):
+            people[j] = people[j] + people[j - 1]
+
+    print(people[-1])
+    
+    # 답만 필요하니까 생성 X / 쌓아나가면 된다!...
+'''
