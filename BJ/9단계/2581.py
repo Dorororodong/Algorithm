f13f7bdd9376c06c33a22967e19c4b76e5be8b06
LDJ <ehdwnsdl1210@gmail.com>
2022-01-21 21:47:55 +0900

    220121 commit

--- BJ/9단계/2581.py
+++ BJ/9단계/2581.py
@@ -1,13 +1,38 @@
-'''
-자연수 M과 N이 주어질 때 M이상 N이하의 자연수 중 소수인 것을 모두 골라 이들 소수의 합과 최솟값을 찾는 프로그램을 작성하시오.
+import sys
+import math
 
-예를 들어 M=60, N=100인 경우 60이상 100이하의 자연수 중 소수는 61, 67, 71, 73, 79, 83, 89, 97 총 8개가 있으므로,
-이들 소수의 합은 620이고, 최솟값은 61이 된다.
+def discriminate_prime_num(n):
+    global prime_num
+    global min_prime_num
 
-입력의 첫째 줄에 M이, 둘째 줄에 N이 주어진다.
-M과 N은 10,000이하의 자연수이며, M은 N보다 작거나 같다.
+    if n == 1:
+        return
 
-M이상 N이하의 자연수 중 소수인 것을 모두 찾아 첫째 줄에 그 합을, 둘째 줄에 그 중 최솟값을 출력한다.
-단, M이상 N이하의 자연수 중 소수가 없을 경우는 첫째 줄에 -1을 출력한다.
-'''
+    elif n == 2 or n == 3:
+        prime_num += n
+        if min_prime_num == -1:
+            min_prime_num = n
 
+    else:
+        for j in range(2, int(math.sqrt(n))+1):
+            if n % j == 0:
+                return
+        prime_num += n
+        if min_prime_num == -1:
+            min_prime_num = n
+
+M = int(sys.stdin.readline().rstrip())
+N = int(sys.stdin.readline().rstrip())
+
+prime_num = 0
+min_prime_num = -1
+
+for i in range(M, N+1):
+    discriminate_prime_num(i)
+
+if prime_num == 0:
+    print(min_prime_num)
+
+else:
+    print(prime_num)
+    print(min_prime_num)
