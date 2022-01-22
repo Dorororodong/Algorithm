381c7ba76acb64d4863340774b57d41aaacdad96
LDJ <ehdwnsdl1210@gmail.com>
2022-01-22 21:11:34 +0900

    220122 commit

--- BJ/9단계/11653.py
+++ BJ/9단계/11653.py
@@ -1,7 +1,18 @@
-'''
-정수 N이 주어졌을 때, 소인수분해하는 프로그램을 작성하시오.
+import sys
+import math
 
-첫째 줄에 정수 N (1 ≤ N ≤ 10,000,000)이 주어진다.
+N = int(sys.stdin.readline().rstrip())
 
-N의 소인수분해 결과를 한 줄에 하나씩 오름차순으로 출력한다. N이 1인 경우 아무것도 출력하지 않는다.
-'''
+while N != 1:
+    if N == 2 or N == 3:
+        print(N)
+        break
+    else:
+        for i in range(2, int(math.sqrt(N)) + 1):
+            while N % i == 0:
+                N //= i
+                print(i)
+
+        if i == int(math.sqrt(N)):
+            print(N)
+            break
