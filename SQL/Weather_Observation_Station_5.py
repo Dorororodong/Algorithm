5bd5e9eb863d0d5ad0f22c604fd6ed14fb726d92
LDJ <ehdwnsdl1210@gmail.com>
2022-02-10 21:04:27 +0900

    🤪220210 commit SQL

---
+++ SQL/Weather_Observation_Station_5.py
@@ -0,0 +1,8 @@
+'''
+Select CITY, Length(CITY) From STATION Order By Length(CITY) ASC, CITY ASC Limit 1;
+Select CITY, Length(CITY) From STATION Order By Length(CITY) DESC, CITY ASC Limit 1;
+2를 원하기도 하고, 2개로 따로 출력하기도 한다!
+Length: 길이!!
+ASC, DESC
+Limit!
+'''
