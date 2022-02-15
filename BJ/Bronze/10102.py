e086db3f38bde30788af3513f4a62300e11bf6d2
LDJ <ehdwnsdl1210@gmail.com>
2022-02-15 22:48:51 +0900

    220215 commit

---
+++ BJ/Bronze/10102.py
@@ -0,0 +1,12 @@
+import sys
+V = int(sys.stdin.readline().rstrip())
+vote = list(sys.stdin.readline().rstrip())
+
+if vote.count('A') > vote.count('B'):
+    print('A')
+
+elif vote.count('A') < vote.count('B'):
+    print('B')
+
+else:
+    print('Tie')
