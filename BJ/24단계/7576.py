611c286e98a0cbfe36b39b2c62161e37cfbb3d94
LDJ <ehdwnsdl1210@gmail.com>
2022-02-22 23:22:31 +0900

    🤪💀220222 commit

--- BJ/24단계/7576.py
+++ BJ/24단계/7576.py
@@ -1,55 +1,78 @@
-'''
-창고에 보관되는 토마토들 중에는 잘 익은 것도 있지만, 아직 익지 않은 토마토들도 있을 수 있다.
-보관 후 하루가 지나면, 익은 토마토들의 인접한 곳에 있는 익지 않은 토마토들은 익은 토마토의 영향을 받아 익게 된다.
-하나의 토마토의 인접한 곳은 왼쪽, 오른쪽, 앞, 뒤 네 방향에 있는 토마토를 의미한다.
-대각선 방향에 있는 토마토들에게는 영향을 주지 못하며, 토마토가 혼자 저절로 익는 경우는 없다고 가정한다.
-철수는 창고에 보관된 토마토들이 며칠이 지나면 다 익게 되는지, 그 최소 일수를 알고 싶어 한다.
-
-토마토를 창고에 보관하는 격자모양의 상자들의 크기와 익은 토마토들과 익지 않은 토마토들의 정보가 주어졌을 때,
-며칠이 지나면 토마토들이 모두 익는지, 그 최소 일수를 구하는 프로그램을 작성하라. 단, 상자의 일부 칸에는 토마토가 들어있지 않을 수도 있다.
-
-첫 줄에는 상자의 크기를 나타내는 두 정수 M,N이 주어진다.
-M은 상자의 가로 칸의 수, N은 상자의 세로 칸의 수를 나타낸다.
-단, 2 ≤ M,N ≤ 1,000 이다.
-
-둘째 줄부터는 하나의 상자에 저장된 토마토들의 정보가 주어진다.
-즉, 둘째 줄부터 N개의 줄에는 상자에 담긴 토마토의 정보가 주어진다.
-하나의 줄에는 상자 가로줄에 들어있는 토마토의 상태가 M개의 정수로 주어진다.
-정수 1은 익은 토마토, 정수 0은 익지 않은 토마토, 정수 -1은 토마토가 들어있지 않은 칸을 나타낸다.
-
-토마토가 하나 이상 있는 경우만 입력으로 주어진다.
-
-여러분은 토마토가 모두 익을 때까지의 최소 날짜를 출력해야 한다.
-만약, 저장될 때부터 모든 토마토가 익어있는 상태이면 0을 출력해야 하고,
-토마토가 모두 익지는 못하는 상황이면 -1을 출력해야 한다.
-'''
-
+# BFS 기본형에 너무 몰입... 결국 동시에 진행하는 경우가 있으니 Queue의 특성을 이용해서 선입선출...
 dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
 
-def BFS(x, y):
-    global cnt
-    if tomato[x][y] == 1:
+def BFS():
+    global res
+    while ripe:
+        x, y = ripe.popleft()
 
-    else:
+        # 여기서 대소비교를 미리 해버리면?
+        if tomato[x][y] > res:
+            res = tomato[x][y]
+
         for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
 
-
+            if 0 <= nx < N and 0 <= ny < M:
+                if tomato[nx][ny] == 0:
+                    tomato[nx][ny] = tomato[x][y] + 1
+                    ripe.append((nx, ny))
 
 import sys
+from collections import deque
 
-M, N = map(int, sys.stdin.readline().rstrip().split())  # M : 가로 / N : 세로
-
+M, N = map(int, sys.stdin.readline().rstrip().split())
 tomato = [list(map(int, sys.stdin.readline().rstrip().split())) for _ in range(N)]
-visited = [[0] * M for _ in range(N)]
-# print(tomato)
+ripe = deque()
+for i in range(N):
+    for j in range(M):
+        if tomato[i][j] == 1:
+            ripe.append((i, j))
 
-cnt = 0
+res = 0
+BFS()
+for i in tomato:
+    for j in i:
+        if j == 0:
+            print(-1)
+            exit()
+print(res - 1)
 
-for i in range(M):
-    for j in range(N):
-        if (tomato[i][j] == 1 or tomato[i][j] == 0) and visited[i][j] == 0:
-            BFS(i, j)
+'''
+# 함수를 선언해서 안되는 경우 체크!
+import sys
+from collections import deque
+_max = 0
+M, N = map(int, sys.stdin.readline().split())
+BOARD = [ list(map(int, sys.stdin.readline().split())) for _ in range(N) ]
+queue = deque()
+vectorX = [1, -1, 0, 0]
+vectorY = [0, 0, 1, -1]
 
-# 0 인데 전부 -1 이면 -1 출력
-# 1 이면 주변 0을 1로 BFS 돌면서 1일 추가, 없으면 0 이겠지
+def checkBoard():
+    for i in range(N):
+        for j in range(M):
+            if BOARD[i][j] == 0:
+                return False
+    return True
+
+for i in range(N):
+    for j in range(M):
+        if BOARD[i][j] == 1:
+            queue.append((i, j))
+
+while queue:
+    X, Y = queue.popleft()
+    if BOARD[X][Y] > _max:
+        _max = BOARD[X][Y]
+
+    for i in range(4):
+        dX, dY = X + vectorX[i], Y + vectorY[i]
+        if 0 <= dX < N and 0 <= dY < M and BOARD[dX][dY] == 0:
+            BOARD[dX][dY] = BOARD[X][Y] + 1
+            queue.append((dX, dY))
+
+print(_max-1 if checkBoard() else -1)
+'''
