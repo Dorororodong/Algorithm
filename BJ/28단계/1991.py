1a883db1fa960f36897eb85302939355a9b9985d
LDJ <ehdwnsdl1210@gmail.com>
2022-03-01 19:36:14 +0900

    220301 commit

--- BJ/28단계/1991.py
+++ BJ/28단계/1991.py
@@ -1,42 +1,35 @@
-# 전위 순회 (V -> L -> R)
-def pre_order(node, n):
+def pre_order(node):
     if node != '.':
         print(node, end='')
-        pre_order(tree[n+1][0], n+1)
-        pre_order(tree[n+2][1], n+2)
+        pre_order(tree[ord(node) - 65][1])
+        pre_order(tree[ord(node) - 65][2])
 
-    return
 
-# 중위 순회 (L -> V -> R)
-def in_order(node, n):
+def in_order(node):
     if node != '.':
-        pre_order(tree[n+1][0], n+1)
+        in_order(tree[ord(node) - 65][1])
         print(node, end='')
-        pre_order(tree[n+2][1], n+2)
+        in_order(tree[ord(node) - 65][2])
 
-    return
 
-# 후위 순회 (L -> R -> V)
-def post_order(node, n):
+def post_order(node):
     if node != '.':
-        pre_order(tree[n+1][0], n+1)
-        pre_order(tree[n+2][1], n+2)
+        post_order(tree[ord(node) - 65][1])
+        post_order(tree[ord(node) - 65][2])
         print(node, end='')
 
-    return
 
 import sys
-sys.stdin = open('input.txt')
+N = int(sys.stdin.readline().rstrip())
+tree = [[0] * 3 for _ in range(N)]
 
-V = int(input())
+for _ in range(N):
+    P, L, R = sys.stdin.readline().rstrip().split()
+    tree[ord(P) - 65][1] = L
+    tree[ord(P) - 65][2] = R
 
-tree = [[0] * 3 for _ in range(V)]
-
-for tc in range(V):
-    P, L, R = input().split()
-    tree[tc][0] = L
-    tree[tc][1] = R
-
-print(pre_order('A', 0))
-print(in_order('A', 0))
-print(post_order('A', 0))
+pre_order('A')
+print()
+in_order('A')
+print()
+post_order('A')
