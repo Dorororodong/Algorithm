c1abd1af782989710104c07859ec7a7c625cb04e
LDJ <ehdwnsdl1210@gmail.com>
2022-03-26 23:02:52 +0900

    BJ file restructured commit

--- BJ/3_반복문/1110.py
+++ BJ/3_반복문/1110.py
@@ -3,13 +3,13 @@ cnt = 0
 S = N
 
 while S != N or cnt == 0:
-    cnt += 1 
+    cnt += 1
     if N < 10:
         N = 10 * N + N
-        
+
     else:
-        N = 10 * (N%10) + (((N%10) + (N//10))%10)
-        
+        N = 10 * (N % 10) + (((N % 10) + (N // 10)) % 10)
+
 print(cnt)
 
 # n = int(input())
