bd092546e8f473148f645eac3b422744d56efea1
LDJ <ehdwnsdl1210@gmail.com>
2022-04-07 22:18:27 +0900

    🤪220407 commit SQL

---
+++ PG/SQL/Lv.2/DATETIME에서_DATE로_형_변환.py
@@ -0,0 +1,8 @@
+'''
+Select ANIMAL_ID, NAME, Date_Format(DATETIME, '%Y-%m-%d')
+From ANIMAL_INS
+Order By ANIMAL_ID;
+
+# DATE_FORMAT(DATE, 형식)
+# 형식에는 %Y(4자리 연도), %y(2자리 연도), %m(월), %d(일), %H(24시간), %h(12시간), %i, %s가 있음
+'''
