e714535f42586256e7bf0ae50a78bb9bb0cf7cae
LDJ <ehdwnsdl1210@gmail.com>
2022-05-24 14:29:11 +0900

    Revert "220524 commit SQL"
    This reverts commit 9faec9b1576b5c7ebeecde11c348dbef08880b9d.

--- LeetCode/Database/184.sql
+++
@@ -1,5 +0,0 @@
-SELECT d.name as Department, e.name as Employee, e.salary
-FROM Employee as e
-INNER JOIN Department as d
-ON e.departmentId = d.id
-WHERE (e.departmentId, e.salary) in (SELECT departmentId, MAX(salary) FROM Employee GROUP BY departmentId);
