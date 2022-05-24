9faec9b1576b5c7ebeecde11c348dbef08880b9d
LDJ <ehdwnsdl1210@gmail.com>
2022-05-24 14:28:05 +0900

    220524 commit SQL

---
+++ LeetCode/Database/184.sql
@@ -0,0 +1,5 @@
+SELECT d.name as Department, e.name as Employee, e.salary
+FROM Employee as e
+INNER JOIN Department as d
+ON e.departmentId = d.id
+WHERE (e.departmentId, e.salary) in (SELECT departmentId, MAX(salary) FROM Employee GROUP BY departmentId);
