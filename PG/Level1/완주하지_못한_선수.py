95da436f89ee7f81e01a1588307c0ddcc2d2ef50
LDJ <ehdwnsdl1210@gmail.com>
2023-02-17 23:50:50 +0900

    🤪230217 commit

--- PG/Level1/완주하지_못한_선수.py
+++ PG/Level1/완주하지_못한_선수.py
@@ -1,24 +1,16 @@
-'''
-수많은 마라톤 선수들이 마라톤에 참여하였습니다.
-단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주하였습니다.
-마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때,
-완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
-
-마라톤 경기에 참여한 선수의 수는 1명 이상 100,000명 이하입니다.
-completion의 길이는 participant의 길이보다 1 작습니다.
-참가자의 이름은 1개 이상 20개 이하의 알파벳 소문자로 이루어져 있습니다.
-참가자 중에는 동명이인이 있을 수 있습니다.
-'''
+from collections import defaultdict
 
 def solution(participant, completion):
+    p_set = defaultdict(int)
+
     for i in participant:
-        if i not in completion:
-            return i
+        p_set[i] += 1
 
-        else:
-            if participant.count(i) != completion.count(i):
-                return i
+    for j in completion:
+        p_set[j] -= 1
 
-print(solution(["leo", "kiki", "eden"], ["eden", "kiki"]))                                                          # "leo"
-print(solution(["marina", "josipa", "nikola", "vinko", "filipa"], ["josipa", "filipa", "marina", "nikola"]))        # "vinko"
-print(solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]))                                 # "mislav"
+    for (k, l) in p_set.items():
+        if l > 0:
+            return k
+
+print(solution(["leo", "kiki", "eden"], ["eden", "kiki"]))
