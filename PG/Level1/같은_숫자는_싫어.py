2fa6336b564346a5ce7bfc697eb2f0886d86eec5
LDJ <ehdwnsdl1210@gmail.com>
2023-02-18 16:26:47 +0900

    230218 commit

--- PG/Level1/같은_숫자는_싫어.py
+++ PG/Level1/같은_숫자는_싫어.py
@@ -1,28 +1,18 @@
-'''
-배열 arr의 각 원소는 숫자 0부터 9까지로 이루어져 있습니다.
-이때, 배열 arr에서 연속적으로 나타나는 숫자는 하나만 남기고 전부 제거하려고 합니다.
-단, 제거된 후 남은 수들을 반환할 때는 배열 arr의 원소들의 순서를 유지해야 합니다.
-
-arr = [1, 1, 3, 3, 0, 1, 1] 이면 [1, 3, 0, 1] 을 return 합니다.
-배열 arr에서 연속적으로 나타나는 숫자는 제거하고 남은 수들을 return 하는 solution 함수를 완성해 주세요.
-
-배열 arr의 크기 : 1,000,000 이하의 자연수
-배열 arr의 원소의 크기 : 0보다 크거나 같고 9보다 작거나 같은 정수
-'''
-
 def solution(arr):
-    number = -1
-    result = []
+    stack = []
+
     for i in arr:
-        if number == -1:
-            number = i
-            result.append(i)
+        if stack:
+            if stack[-1] == i:
+                continue
+
+            else:
+                stack.append(i)
+
         else:
-            if number != i:
-                number = i
-                result.append(i)
+            stack.append(i)
 
-    return result
+    return stack
 
-print(solution([1,1,3,3,0,1,1]))        # [1,3,0,1]
-print(solution([4,4,4,3,3]))            # [4,3]
+print(solution([1,1,3,3,0,1,1]))
+print(solution([4,4,4,3,3]))
